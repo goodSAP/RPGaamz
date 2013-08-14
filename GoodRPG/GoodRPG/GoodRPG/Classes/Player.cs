c151d7701b64882d97addcde416423b21a02e308
Solus <solus_94@live.se>
2013-08-14 11:58:33 +0200

    added some using

--- GoodRPG/GoodRPG/GoodRPG/Classes/Player.cs
+++ GoodRPG/GoodRPG/GoodRPG/Classes/Player.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using tile_r;
 using Microsoft.Xna.Framework.Input;
 
