75b09f7db94388256a0f548b7ba0014bf329d148
Solus <solus_94@live.se>
2013-08-14 11:59:39 +0200

    Removed duplicate using

--- GoodRPG/GoodRPG/GoodRPG/Classes/Player.cs
+++ GoodRPG/GoodRPG/GoodRPG/Classes/Player.cs
@@ -6,7 +6,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using tile_r;
-using Microsoft.Xna.Framework.Input;
 
 
 namespace Shooter
